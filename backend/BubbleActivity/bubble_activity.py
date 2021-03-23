4a87be3f423a2b8b39adaf56414f479dca41cfc5
Brandon <brandu.tan@gmail.com>
2021-03-23 17:45:13 +0800

    Added in feature of getting email to send

--- backend/BubbleActivity/bubble_activity.py
+++ backend/BubbleActivity/bubble_activity.py
@@ -213,9 +213,15 @@ def join_bubble():
         return unavailable_callback("Bubble Roles")
 
     if create_roles_response.status_code in range(200,300):
+        bubble_id = request.get_json()['bubble_id']
+        bubble_participants = requests.request(method="GET",url=br_bubble_participant+str(bubble_id)).json()["data"]
+        emails = list(bubble_participants.keys())
         return jsonify({
                 "code":201,
-                "message":"bubble join success"
+                "message":"bubble join success",
+                "data" : {
+                    "emails":emails
+                }
             }),201
     else:
         return error_callback(create_roles_response.json())
